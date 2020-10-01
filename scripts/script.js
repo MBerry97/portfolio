34b28898984b1abc3ddf0cd5f3f2cd039b032259
MBerry97 <imatty2011@hotmail.co.uk>
2020-10-01 22:29:16 +0100

    Added animation to the adjust and footer of index

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,19 @@
 const adjustIcon = document.querySelector("#adjust");
 const background = document.querySelector("body");
-const navBar = document.querySelector(".navigation ul li");
+const navBar = document.querySelectorAll(".navlist li");
 
 adjustIcon.addEventListener("click", () => {
   background.classList.toggle("change");
-  navBar.classList.toggle("change");
+  // navBar.classList.toggle("change");
   console.log(navBar);
 });
 
 // for (user of navBar) {
-//   user.addEventListener("click", function () {
-//     console.log(this);
+//   adjustIcon.addEventListener("click", function () {
+//     console.log(user);
 //   });
 // }
+
+//Sides of index para slide in when page loads
+
+//projects page, organise steps with captions either side and a invsible box underneats that highlights when u hover over
