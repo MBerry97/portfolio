254cf3baa9bf5e8cc8effc73cd618f2c15f91acf
MBerry97 <imatty2011@hotmail.co.uk>
2020-10-02 18:23:26 +0100

    sorted timings of index animations

--- scripts/script.js
+++ scripts/script.js
@@ -10,13 +10,13 @@ adjustIcon.addEventListener("click", () => {
 
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".text", { y: "0%", duration: 1.5, stagger: 0.5 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo(".main", { opacity: 0 }, { opacity: 1, duration: 2 });
+tl.fromTo(".main p", { opacity: 0 }, { opacity: 1, duration: 2 });
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo("footer", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo("#adjust", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".socials i", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo("#adjust", { opacity: 0 }, { opacity: 1, duration: 2 });
 //Sides of index para slide in when page loads
 
 //projects page, organise steps with captions either side and a invsible box underneats that highlights when u hover over
