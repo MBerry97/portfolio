6f29a91a14f71aaed2aa0660905c1c4c6d580706
MBerry97 <imatty2011@hotmail.co.uk>
2020-10-02 14:56:03 +0100

    loading index animation with slider and intro text

--- scripts/script.js
+++ scripts/script.js
@@ -8,12 +8,15 @@ adjustIcon.addEventListener("click", () => {
   console.log(navBar);
 });
 
-// for (user of navBar) {
-//   adjustIcon.addEventListener("click", function () {
-//     console.log(user);
-//   });
-// }
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo(".main", { opacity: 0 }, { opacity: 1, duration: 2 });
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo("footer", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo("#adjust", { opacity: 0 }, { opacity: 1, duration: 1 });
 //Sides of index para slide in when page loads
 
 //projects page, organise steps with captions either side and a invsible box underneats that highlights when u hover over
