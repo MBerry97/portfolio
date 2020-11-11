bbd3fe7e1c2bbe84e65e8edadc236354bd81dce3
MBerry97 <imatty2011@hotmail.co.uk>
2020-11-11 16:34:24 +0000

    added project slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const adjustIcon = document.querySelector("#adjust");
 const background = document.querySelector("body");
-const navBar = document.querySelectorAll(".navlist li");
+
 
 
 const aboutBorder = document.querySelector("navigation");
@@ -12,10 +12,9 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to(".text", { y: "0%", duration: 1.5, stagger: 0.5 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo(".main p", { opacity: 0 }, { opacity: 1, duration: 2 });
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".socials i", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo("#adjust", { opacity: 0 }, { opacity: 1, duration: 2 });
+tl.fromTo(".main p", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".socials i", { opacity: 0 }, { opacity: 1, duration: 0.5 });
+tl.fromTo("#adjust", { opacity: 0 }, { opacity: 1, duration: 1 });
 //Sides of index para slide in when page loads
 
 //projects page, organise steps with captions either side and a invsible box underneats that highlights when u hover over
