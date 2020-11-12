8a74549f884fa640fecaa8372bfa7b55493cb24c
MBerry97 <imatty2011@hotmail.co.uk>
2020-11-12 19:56:21 +0000

    media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,6 @@
 const adjustIcon = document.querySelector("#adjust");
 const background = document.querySelector("body");
 
-
-
-const aboutBorder = document.querySelector("navigation");
-
-
-
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1.5, stagger: 0.5 });
@@ -15,17 +9,12 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo(".main p", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".socials i", { opacity: 0 }, { opacity: 1, duration: 0.5 });
 tl.fromTo("#adjust", { opacity: 0 }, { opacity: 1, duration: 1 });
-//Sides of index para slide in when page loads
-
-//projects page, organise steps with captions either side and a invsible box underneats that highlights when u hover over
+tl.fromTo(".box", { opacity: 0 }, { opacity: 1, duration: 1 });
 
 adjustIcon.addEventListener("click", () => {
   background.classList.toggle("change");
-  // navBar.classList.toggle("change");
-  // aboutBorder.classList.toggle("change");
+ 
 });
-
-
 
 var flkty = new Flickity( '.carousel', {
   cellAlign: 'left',
